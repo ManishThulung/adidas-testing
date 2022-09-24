4c450f13975be6ede5ee2d5aca64e4637129d8c6
Manish Thulung <manish.thulung58@gmail.com>
2022-09-24 17:20:48 +0545

    login route corrected

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,9 +17,12 @@ router.get(
   })
 );
 
-router.get("/login", passport.authenticate("google"), {
-  successRedirect: process.env.FRONTEND_URL,
-});
+router.get(
+  "/login",
+  passport.authenticate("google", {
+    successRedirect: process.env.FRONTEND_URL,
+  })
+);
 // router.get(
 //   "/login",
 //   passport.authenticate("google", {
